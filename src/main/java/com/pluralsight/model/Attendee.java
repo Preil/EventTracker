27a3eb4883e17e7064c9c175aa3cb4a132de9ce6
Ilya Preobrazhenskiy <preil2000@gmail.com>
2017-05-03 06:56:43 +0300

    model added.

---
+++ src/main/java/com/pluralsight/model/Attendee.java
@@ -0,0 +1,34 @@
+package com.pluralsight.model;
+
+/**
+ * Ilya 03.05.2017.
+ */
+public class Attendee {
+    private String name;
+    private String emailAddress;
+    private String phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
---
+++ src/main/java/com/pluralsight/model/Event.java
@@ -0,0 +1,16 @@
+package com.pluralsight.model;
+
+/**
+ * Ilya 03.05.2017.
+ */
+public class Event {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
